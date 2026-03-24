d768c797b4195ae40a4c51e57daab7dd4b0a0aa0
Chetan Meniya <chetan.meniya@bacancy.com>
2026-03-24 17:08:00 +0530

    updates

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '66ee4009a9f43cc93cc65d2e45385d65613d9024',
+        'reference' => '0cbb5525c07fe3afd5878749fd3eaafb1d4a4deb',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -400,7 +400,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '66ee4009a9f43cc93cc65d2e45385d65613d9024',
+            'reference' => '0cbb5525c07fe3afd5878749fd3eaafb1d4a4deb',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
